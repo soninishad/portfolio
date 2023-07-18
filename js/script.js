81beb2a109be747365fb642338d64ae6774cad96
Soni Nishad <93151858+soninishad@users.noreply.github.com>
2023-07-18 16:50:24 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text" , {
+    strings:["Web Developer" , "Software Developer" , "PHP Developer" , "Backend Developer", "Frontend Developer" ] ,
+    typeSpeed:100,
+    backspeed : 100,
+    backDelay:1000,
+    loop:true
+});
